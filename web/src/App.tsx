46d94090238d413332cfb5b935543de26fdeb6a3
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-10-31 13:03:47 +0100

    add web repo

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import { Component, h } from "preact";
+
+interface AppProps {}
+interface AppState {}
+export default class App extends Component<AppProps, AppState> {
+    render = () => {
+        return <div className="App"></div>;
+    };
+}
