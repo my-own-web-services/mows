52aac30a44a7d84802b2ae2d4b23473a00b54117
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2024-06-04 19:21:09 +0200

    move qemu inside its own docker container

--- manager/misc/init-libvirtd.sh
+++ manager/misc/init-libvirtd.sh
@@ -9,7 +9,24 @@ listen_addr = "0.0.0.0"
 auth_tcp = "none"
 EOF
 
+cat > /etc/libvirt/qemu.conf <<EOF
+vnc_listen = "0.0.0.0"
+spice_listen = "0.0.0.0"
+EOF
 
-# Start the libvirtd daemon with config file
+
+# Start libvirtd daemonized then create the default network then stop it and bring it to the foreground
+/usr/sbin/libvirtd --config /etc/libvirt/libvirtd.conf --listen -d
+
+# Create the default network
+virsh net-define /etc/libvirt/qemu/networks/default.xml
+virsh net-start default
+virsh net-autostart default
+
+# Stop libvirtd
+killall libvirtd
+
+
+# Bring libvirtd to the foreground
 /usr/sbin/libvirtd --config /etc/libvirt/libvirtd.conf --listen
 
