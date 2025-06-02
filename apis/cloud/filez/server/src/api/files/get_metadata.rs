12a4290e74497371f37b678ada3ea44fd6b3db7f
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@vindelicum.eu>
2025-06-03 01:19:34 +0200

    add owner auth

--- apis/cloud/filez/server/src/api/files/get_metadata.rs
+++ apis/cloud/filez/server/src/api/files/get_metadata.rs
@@ -16,12 +16,33 @@ use crate::{
     )
 )]
 pub async fn get_files_metadata(
-    user: IntrospectedUser,
+    external_user: IntrospectedUser,
     State(app_state): State<AppState>,
     Json(req_body): Json<GetFilesMetaRequestBody>,
 ) -> Json<ApiResponse<GetFileMetaResBody>> {
+    let user= match app_state
+        .db
+        .get_user_by_external_id(&external_user.user_id)
+        .await
+    {
+        Ok(Some(u)) => u,
+        Ok(None) => {
+            return Json(ApiResponse {
+                status: ApiResponseStatus::Error,
+                message: "User not found".to_string(),
+                data: None,
+            });
+        }
+        Err(e) => {
+            return Json(ApiResponse {
+                status: ApiResponseStatus::Error,
+                message: format!("Database error: {}", e),
+                data: None,
+            });
+        }
+    };
 
-    let files_meta_result=match app_state.db.get_files_metadata(&req_body).await{
+    let files_meta_result=match app_state.db.get_files_metadata_for_owner(&req_body.file_ids,user.user_id).await{
         Ok(files_meta) => files_meta,
         Err(e) => {
             return Json(ApiResponse {
--- apis/cloud/filez/server/src/api/users/apply.rs
+++ apis/cloud/filez/server/src/api/users/apply.rs
@@ -1,4 +1,7 @@
 use axum::{extract::State, Json};
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+use uuid::Uuid;
 use zitadel::axum::introspection::IntrospectedUser;
 
 use crate::types::{ApiResponse, ApiResponseStatus, AppState};
@@ -10,14 +13,14 @@ use crate::types::{ApiResponse, ApiResponseStatus, AppState};
     )
 )]
 pub async fn apply_user(
-    user: IntrospectedUser,
+    external_user: IntrospectedUser,
     State(app_state): State<AppState>,
 ) -> Json<ApiResponse<ApplyUserResponseBody>> {
     match app_state
         .db
         .apply_user(
-            &user.user_id,
-            &user.preferred_username.unwrap_or("".to_string()),
+            &external_user.user_id,
+            &external_user.preferred_username.unwrap_or("".to_string()),
         )
         .await
     {
@@ -34,7 +37,7 @@ pub async fn apply_user(
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize, utoipa::ToSchema, Clone)]
+#[derive(Serialize, Deserialize, ToSchema, Clone)]
 pub struct ApplyUserResponseBody {
-    pub user_id: uuid::Uuid,
+    pub user_id: Uuid,
 }
